361b5ad2413f1b6055d2d6f23af3daebf62ae215
Vadim <vadim.kurn@gmail.com>
2020-11-30 10:34:20 +0300

    headless mode added

--- src/test/java/ru/netology/apporder/AppOrderTest.java
+++ src/test/java/ru/netology/apporder/AppOrderTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -21,7 +22,10 @@ public class AppOrderTest {
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("window-size=1366x768");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
